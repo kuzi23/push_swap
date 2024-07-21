143bd0969f4df3799cf187344a621fcba7499462
Marius Tresor Kwizera <mkwizera@c3r6s1.42warsaw.pl>
2024-07-21 15:27:24 +0200

    good

--- pushswap/move _a_to_b.c
+++ pushswap/move _a_to_b.c
@@ -1,35 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   move _a_to_b.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkwizera <mkwizera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/20 18:20:15 by mkwizera          #+#    #+#             */
-/*   Updated: 2024/07/21 15:10:50 by mkwizera         ###   ########.fr       */
+/*   Created: 2024/07/21 14:19:43 by mkwizera          #+#    #+#             */
+/*   Updated: 2024/07/21 14:27:00 by mkwizera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	push_swap(node **a, node **b)
+void	rotate_both(node **a,node **b, node* cheapest_node)
 {
-	int	lenstack_a;
-
-	lenstack_a = stack_len(*a);
-	if (lenstack_a-- > 3 && (stack_sorted(*a) == false))
-		pa(a, b, true);
-	if (lenstack_a-- > 3 && (stack_sorted(*a) == false))
-		pa(a, b, true);
-	while (lenstack_a > 3 && (stack_sorted(*a) == false))
+	while (!(b->target_node->cheapest))
 	{
-		init_a(*a, *b);
-		move_a_to_b(*a, *b);
-        lenstack_a--;
-	}
-	sort_three(a);
-	while (!(b))
-	{
-		init_a();
+        
 	}
 }
